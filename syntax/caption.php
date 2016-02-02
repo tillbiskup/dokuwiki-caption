af7d3c1c830602f005e0bef71e7eb69f843c8c77
Andreas Gohr <andi@splitbrain.org>
2016-02-02 13:01:20 +0100

    Adjust method signatures to match parent

--- syntax/caption.php
+++ syntax/caption.php
@@ -70,7 +70,7 @@ class syntax_plugin_caption_caption extends DokuWiki_Syntax_Plugin {
         $this->Lexer->addExitPattern('</table>','plugin_caption_caption');
     }
 
-    public function handle($match, $state, $pos, &$handler){
+    public function handle($match, $state, $pos, Doku_Handler $handler){
         switch ($state) {
           case DOKU_LEXER_ENTER :
             $match = substr($match,1,-1);
@@ -84,7 +84,7 @@ class syntax_plugin_caption_caption extends DokuWiki_Syntax_Plugin {
         return array();
     }
 
-    public function render($mode, &$renderer, $data) {
+    public function render($mode, Doku_Renderer $renderer, $data) {
         if ($mode == 'xhtml') {
 
             list($state,$match) = $data;
