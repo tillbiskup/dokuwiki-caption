04a57734b263ad8ff834ee0d1a8f120486902a0e
kabbala <kabbala@users.noreply.github.com>
2020-11-03 14:40:31 +0900

    typo

--- lang/ko/lang.php
+++ lang/ko/lang.php
@@ -6,4 +6,4 @@ $lang['tablelong'] = '표';
 $lang['tableabbrev'] = '표';
 $lang['figure'] = '이미지와 캡션 함께 넣기';
 $lang['table'] = '표와 캡션 함께 넣기';
-$lang['reference'] = 이미지/표의 각주 넣기';
+$lang['reference'] = '이미지/표의 각주 넣기';
